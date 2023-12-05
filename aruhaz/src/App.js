6fbf4d700c31e87f10d5a251f2a07ffa6bac4ddc
Wendesday36 <bertazoeanna@gmail.com>
2023-12-05 11:43:28 +0100

    elmeletileg mukodo kosar

--- aruhaz/src/App.js
+++ aruhaz/src/App.js
@@ -2,9 +2,15 @@
 import './App.css';
 import Termekek from './Termekek';
 import {termekek} from "./KepLista";
-
+import  {useState} from 'react';
 function App() {
-  
+  const [aktTermek,setAktTermek]=useState(0) 
+    function kattintas(index) {
+    
+        console.log(index);
+        setAktTermek(index) 
+        /*itt kapja a gyerekkomponenstol az adatot*/
+      }
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +19,7 @@ function App() {
       <aside>
       <Termekek obj ={termekek[aktTermek]}/></aside>
       <article>
-        <Termekek />
+        <Termekek kattintas={kattintas} />
       </article>
       <footer>Berta Zoé Anna</footer>
     </div>
--- aruhaz/src/Termekek.js
+++ aruhaz/src/Termekek.js
@@ -1,12 +1,12 @@
 import Termek from "./Termek";
 import {termekek} from "./KepLista";
-import  {useState} from 'react';
-export default function Termekek(){
-    const [aktTermek,setAktTermek]=useState(0) 
+
+export default function Termekek(props){
     function kattintas(index) {
     
         console.log(index);
-        setAktTermek(index) 
+        props.kattintas(index)
+        /* setAktTermek(index) */ 
         /*itt kapja a gyerekkomponenstol az adatot*/
       }
     return(
